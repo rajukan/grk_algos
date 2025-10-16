8e5589e6d818464add65733ac1d23784584de45f
rajukan <gyan.raju@gmail.com>
2025-10-15 22:00:34 -0400

    Document von Neumann's unbiased coin flip algorithm
    Added detailed explanation of von Neumann's method for generating unbiased coin flips using a biased coin.

--- others/unbiased_coin.py
+++ others/unbiased_coin.py
@@ -39,6 +39,32 @@ from collections import Counter
 #
 # This ensures unbiased outcomes.
 
+'''
+More context:
+
+There are many ways to remove the bias from an unfair coin but the most popular method was proposed by the legendary polymath, John “Johnny” Von Neumann. Here is the simple algorithm that von Neumann devised:
+
+    Toss the coin twice
+    If the results match, start over, forgetting both results.
+    If the results differ, use the first result, forgetting the second.
+
+How it works
+
+Assuming the coin flips are independent and the same coin is used in both tosses, the probability of getting different results across tosses will always be the same. That is, P(HT) = P(TH).
+
+Let’s look at a concrete example to buttress this point. Say we have a biased coin that returns heads 80% of the time (p = 0.8). Obviously, this coin will return tails 20% of the time (q = 1-p = 0.2). On two consecutive coin tosses, we will have 1 of these 4 outcomes, [HH, HT, TH, TT] with corresponding probabilities,
+
+P(HH) = p² = 0.8² = 0.64
+
+P(HT) = pq = 0.8 x 0.2 = 0.16
+
+P(TH) = qp = 0.2 x 0.8 = 0.16
+
+P(TT) = q² = 0.2² = 0.04
+
+Notice how the probability for HEADS followed by TAILS is the same as that of TAILS followed by HEADs. This means that we have two equiprobable events, P(HT) = P(TH), that can substitute as the event space for tossing an unbiased coin, (P(H) = P(T) = 0.5). Essentially, when we get HEADS on the first toss and TAILS on the second, we record that as a fair HEADS event. Similarly, when we get TAILS on the first toss and HEADS on the second, we record that as a fair TAILS event.
+'''
+
 def biased_coin_flip():
     p=0.8 #enforce bias]
     return 1 if random.random()<p else 0
