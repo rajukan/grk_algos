5e280daa7d8d1053bf610a3396fde8f0da37f4fb
gyanr <gyan.raju@gmail.com>
2026-01-13 14:46:43 -0500

    candy

--- others/candy.py
+++ others/candy.py
@@ -25,27 +25,32 @@ Explanation: You can allocate to the first, second and third child with 1, 2, 1 
 The third child gets 1 candy because it satisfies the above two conditions.
 
 Trick: Instead of hashmap use candies array
+Trick: Instead of hashmap use candies array
 '''
 
 
 class Solution(object):
     def candy(self, ratings):
         n = len(ratings)
-        cnt = 0
+        #initialize all kids get atleast 1
         candies = [1] * n
+        #compare with previous kid
         for i in range(1, n):
             if ratings[i] > ratings[i - 1]:
                 candies[i] = candies[i - 1] + 1
+        #right to left and compare,
         for i in range(n - 1, 0, -1):
             if ratings[i - 1] > ratings[i]:
+                # As the previous kid has better ratings meaning  either he has to get 1 more  than i ( candies[i]+1) or whatever he holds currently, whichever is maximum
                 candies[i - 1] = max(candies[i] + 1, candies[i - 1])
-            cnt += candies[i - 1]
-        return cnt + candies[n - 1]
+        print(ratings)
+        print(candies)
+        return sum(candies)
 
 
 if __name__ == '__main__':
     solution = Solution()
-    ratings=[1,2,2,]
+    ratings=[1,2,0,4,10]
 
     print(solution.candy(ratings))
 
