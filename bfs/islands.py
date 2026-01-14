fe1ad124b4dff34878843b549570664c1697ff65
gyanr <gyan.raju@gmail.com>
2026-01-13 19:18:08 -0500

    candy

---
+++ bfs/islands.py
@@ -0,0 +1,75 @@
+'''
+
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+
+
+Example 1:
+
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+
+Example 2:
+
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+
+
+'''
+import collections
+from typing import List
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        rows,cols = len(grid),len(grid[0])
+        visit=set()
+        islands=0
+
+        def bfs(subgrid_row,subgrid_c):
+            q=collections.deque()
+            visit.add((subgrid_row,subgrid_c))
+            q.append((subgrid_row,subgrid_c))
+            directions = [(0,1),(0,-1),(1,0),(-1,0)]
+
+            while q:
+                row, col = q.popleft()
+                for dr,dc in directions:
+                    r, c = row+dr, col+dc
+
+                    if (r in range(rows) and
+                            c in range(cols) and
+                            (r,c) not in visit and
+                            grid[r][c] == "1"):
+                            q.append((r,c))
+                            visit.add((r,c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r,c) not in visit:
+                    bfs(r,c)
+                    islands+=1
+        return islands
+
+if __name__ == "__main__":
+    s = Solution()
+    grid = [
+        ["1","1","1","1","0"],
+        ["1","1","0","1","0"],
+        ["1","1","0","0","0"],
+        ["0","0","0","0","0"]
+    ]
+    print(s.numIslands(grid))
