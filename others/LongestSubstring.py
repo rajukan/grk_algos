987893d8b1015e999c7bb25300875ab1181ed703
gyanr <gyan.raju@gmail.com>
2026-01-25 19:32:35 -0500

    all in others

--- others/LongestSubstring.py
+++ others/LongestSubstring.py
@@ -1,7 +1,7 @@
 '''
 
 Input [11111] => Output 1
-Input [123412] => Output 4
+Input [123152] => Output 4, rem we are taking about substrings not subsequences
 
 '''
 
@@ -11,24 +11,24 @@ class Solution:
         n=len(list(s))
         left=0
         max_len=1
-        subarr=[s[left]]
+        bag=[s[left]]
 
         while right<n:
-            if s[left]!= s[right] and s[right] not in subarr:
-                subarr.append(s[right])
-                max_len=max(len(subarr),max_len)
+            if s[left]!= s[right] and s[right] not in bag:
+                bag.append(s[right])
+                max_len=max(len(bag),max_len)
             else:
                 left=right
-                subarr.clear()
-                subarr.append(s[left])
+                bag.clear()
+                bag.append(s[left])
             right+=1
         print(max_len)
-        print(subarr)
+        print(bag)
         return max_len
 
 
 
 if __name__=='__main__':
     t=Solution()
-    t.lengthOfLongestSubstring("123412")
+    t.lengthOfLongestSubstring("123512")
     # t.lengthOfLongestSubstring("1234129012345")
